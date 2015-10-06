1a86b8095d2b18c658edab9e795b8aef0bb2c4bb
hyominchoi <hyomin.choi@gmail.com>
2015-10-06 17:24:02 -0400

    ADD possibility of having an odd num of players in swissParings()

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -103,12 +103,12 @@ def reportMatch(winner, loser):
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -118,15 +118,44 @@ def swissPairings():
     """
     conn = connect()
     cur = conn.cursor()
+
     # We will retur the following paring list in the end
     pairing_list = []
+
     # Construct list of tuples
     l = playerStandings()
+    bye_player = tuple()
+    num_player = len(l)
+
+    # We need to consider the where there is an odd number of players.
+    # free_won is a dictionary that represents set of players who has
+    # won a free game "bye"
+    if (num_player % 2 is 1):
+        free_won = {}
+        cur.execute(""" SELECT winner_id FROM matches WHERE loser_id IS NULL;""")
+        for num in cur.fetchall():
+            free_won[num] = 'yes'
+        # We need to decide to which player we want to give a next bye
+        for i in range(0, num_player):
+            current_player = l[i]
+            # If current player's id is not in the dictionary,
+            # then we give the player a free win
+            if (current_player[0] not in free_won.key()):
+                bye_player = current_player
+                break
+        # We need to remove this player from the list of playerStandings
+        # for the pairing purpose.
+        del l[i]
+        # We record the bye match record for this player
+        cur.execute("""INSERT INTO matches (winner_id) VALUES ('%s');""", [bye_player[0]])
+        conn.commit()
+
+    # We can now pair the players for the next round
     for i in range(0, len(l)/2):
         first_player_info = l[2 * i]
         second_player_info = l[2 * i + 1]
         # Create a tuple of (id1, name1, id2, name2)
-        t = (first_player_info[0], first_player_info[1], 
+        t = (first_player_info[0], first_player_info[1],
              second_player_info[0], second_player_info[1])
         # Concatenate it to the pairing list
         pairing_list += [t]
